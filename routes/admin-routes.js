6ec1c3744950960acc61c2a01ea453d70f9840ad
Ben Hopkins <bjhops17@gmail.com>
2021-02-22 09:23:20 -0800

    added proper hashing and checking

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -3,52 +3,60 @@ const db = require("../models")
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
-
-
-
+//////
 
 router.post("/api/admin/login", async (req,res) => {
-    const data = await db.Admin.findOne({
+
+        
+   
+    const data = await db.Admin.findOne(
+        {
             where: {
-                password: req.body.password,
-                username: req.body.password
+                
+                username: req.body.username
             }
         
         }).catch(err => {
-        res.status(500);
-        console.error(err)
+        res.status(500).end();
+        
     })
-         res.json(data)
-    })
-
-
-
-router.post("/api/admin", async (req,res) => {
-    const hashedPassword = await bcrypt.hashSync(req.body.password, saltRounds)
-    const data = await db.Admin.create({
-        username: req.body.username,
-        password: hashedPassword,
-        email: req.body.email
-    
-    }).catch(err => {
-        res.status(500);
-        console.error(err)
-    })
-         res.status(200)
+        if(password){
+        const match = await bcrypt.compare(req.body.password, data.password);
+        if(match) {
+            res.json(data).status(200).end()
+        }else{
+            throw error
+        }
+    }else{console.log("error .///////////")}
     })
 
 
 
+    router.post("/api/admin", async (req,res) => {
+        const hashedPassword = await bcrypt.hashSync(req.body.password, saltRounds)
+        const data = await db.Admin.create({
+            username: req.body.username,
+            password: hashedPassword,
+            email: req.body.email
+        }).catch(err => {
+            res.status(500);
+            console.error(err)
+        })
+             res.json(data).status(200).end();
+             console.log(data)
+        })
 
 
 
 
-router.put("/api/admin", (req,res) => {
-db.Admin.create(req.body, {
-    where: password = req.body.password
-}).then(data => console.log(res.json(data)))
-})
+
+
+
+// router.put("/api/admin", (req,res) => {
+// db.Admin.create(req.body, {
+//     where: password = req.body.password
+// }).then(data => console.log(res.json(data)))
+// })
 
 
 
--- routes/product-routes.js
+++ routes/product-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const db = require("../models")
 
 
-
+//////Creates Product///////
 router.post("/api/product", async (req,res) => {
 const data = await db.Product.create(req.body).catch(err => {
 
@@ -11,6 +11,50 @@ const data = await db.Product.create(req.body).catch(err => {
 })
      res.json(data)
 })
+
+//////Returns  all Products ///////
+router.get("/api/product", async (req,res) => {
+const data = await db.Product.findAll().catch(err => {
+
+    res.status(500);
+    console.error(err)
+})
+     res.json(data)
+})
+
+//////Returns Product by Category///////
+
+router.get("/api/product/:category", async (req,res) => {
+const data = await db.Product.findAll({
+    where: {
+        category: req.params.category
+    }
+}).catch(err => {
+
+    res.status(500);
+    console.error(err)
+})
+     res.json(data)
+})
+
+////////Returns Fetured Products/////////
+router.get("/api/product/:fetured", async (req,res) => {
+
+const data = await db.Product.findAll(
+    {
+    where: {
+        fetured: req.params.fetured
+    }
+}).catch(err => {
+
+    res.status(500);
+    console.error(err)
+})
+     res.json(data)
+    
+})
+
+
 
 
 
