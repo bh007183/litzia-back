0ea53936ad24718c2f15b04c60db554a676ae2b9
Ben Hopkins <bjhops17@gmail.com>
2021-02-22 09:42:53 -0800

    added error catch

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,56 +1,44 @@
-const router = require("express").Router()
-const db = require("../models")
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 //////
 
-router.post("/api/admin/login", async (req,res) => {
+router.post("/api/admin/login", async (req, res) => {
+    console.log(req.body)
+  const data = await db.Admin.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).catch((err) => {
+    res.status(500).end();
+  });
+ console.log("this is" , data)
+ if(!data){
+     res.send("No Known Username")
+ }
+    const match = await bcrypt.compare(req.body.password, data.password).catch(err => res.status(403).json(err));
+    console.log("this is " , match)
+    res.json(data).status(200).end();
 
-        
-   
-    const data = await db.Admin.findOne(
-        {
-            where: {
-                
-                username: req.body.username
-            }
-        
-        }).catch(err => {
-        res.status(500).end();
-        
-    })
-        if(password){
-        const match = await bcrypt.compare(req.body.password, data.password);
-        if(match) {
-            res.json(data).status(200).end()
-        }else{
-            throw error
-        }
-    }else{console.log("error .///////////")}
-    })
+});
 
+//////
 
-
-    router.post("/api/admin", async (req,res) => {
-        const hashedPassword = await bcrypt.hashSync(req.body.password, saltRounds)
-        const data = await db.Admin.create({
-            username: req.body.username,
-            password: hashedPassword,
-            email: req.body.email
-        }).catch(err => {
-            res.status(500);
-            console.error(err)
-        })
-             res.json(data).status(200).end();
-             console.log(data)
-        })
-
-
-
-
-
-
+router.post("/api/admin", async (req, res) => {
+  const hashedPassword = await bcrypt.hashSync(req.body.password, saltRounds);
+  const data = await db.Admin.create({
+    username: req.body.username,
+    password: hashedPassword,
+    email: req.body.email,
+  }).catch((err) => {
+    res.status(500);
+    console.error(err);
+  });
+  res.json(data).status(200).end();
+  console.log(data);
+});
 
 // router.put("/api/admin", (req,res) => {
 // db.Admin.create(req.body, {
@@ -58,8 +46,4 @@ router.post("/api/admin/login", async (req,res) => {
 // }).then(data => console.log(res.json(data)))
 // })
 
-
-
-
-
-module.exports = router
+module.exports = router;
